e37ae11939b3efc03716768a563114866b64b8bd
Jack Ellis <jackellis1504@gmail.com>
2025-04-09 13:37:40 +0100

    updates to user seeder
     M database/seeders/DatabaseSeeder.php
     M vite.config.ts
    ?? database/seeders/UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!app()->isProduction()) {
+            User::query()->firstOrCreate([
+                'name' => 'Admin',
+                'email' => 'admin@clubhouse.test',
+                'password' => 'password',
+                'email_verified_at' => CarbonImmutable::createFromFormat('Y-m-d H:i:s', '2025-01-01 00:00:00'),
+            ]);
+        }
+    }
+}
