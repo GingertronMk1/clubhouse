8330cdbf3906c65f8aacbaa084e429f6416814c3
Jack Ellis <jackellis1504@gmail.com>
2025-08-26 16:50:14 +0100

    update stubs and some defaults
     M app/Models/User.php
     M database/migrations/0001_01_01_000000_create_users_table.php
     M stubs/migration.create.stub
     M stubs/model.stub
     M stubs/policy.stub
     M stubs/request.stub

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,6 +13,8 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use SoftDeletes;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
