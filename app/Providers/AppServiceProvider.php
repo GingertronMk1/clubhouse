6e8c8bf714a5b4baf010546fd3bbb28c0e7d6896
Jack Ellis <jackellis1504@gmail.com>
2026-09-23 17:28:19 +0100

    Env and appserviceprovider defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -36,6 +37,9 @@ class AppServiceProvider extends ServiceProvider
         DB::prohibitDestructiveCommands(
             app()->isProduction(),
         );
+
+        Model::unguard();
+        Model::shouldBeStrict();
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(12)
