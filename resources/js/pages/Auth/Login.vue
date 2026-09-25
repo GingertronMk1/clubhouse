87dbad0b5dd4d1d3e5da1c4f753d680bda51beff
Jack Ellis <jackellis1504@gmail.com>
2026-09-25 10:41:41 +0100

    NPM fixed
     M resources/css/app.css
     M resources/js/layouts/Layout.vue
     M resources/js/pages/Auth/Login.vue
     M resources/js/pages/Auth/Register.vue
     M resources/js/pages/Welcome.vue

--- resources/js/pages/Auth/Login.vue
+++ resources/js/pages/Auth/Login.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import Layout from "@/layouts/Layout.vue";
-import {router, useForm, usePage} from "@inertiajs/vue3";
-import login from "@/routes/login";
-import {home} from "@/routes";
-import {computed} from "vue";
+import Layout from '@/layouts/Layout.vue';
+import { router, useForm, usePage } from '@inertiajs/vue3';
+import login from '@/routes/login';
+import { home } from '@/routes';
+import { computed } from 'vue';
 
 const loginForm = useForm({
-    email: "",
-    password: "",
-    remember: false
-})
+    email: '',
+    password: '',
+    remember: false,
+});
 
 function submit() {
     loginForm.post(login.store().url, {
-        onSuccess: () => router.visit(home().url)
-    })
+        onSuccess: () => router.visit(home().url),
+    });
 }
 
 const page = usePage();
@@ -27,19 +27,33 @@ const errors = computed(() => page.props.errors ?? {});
             <label for="email">
                 Email
                 {{ errors.email }}
-                <input type="email" name="email" id="email" v-model="loginForm.email"/>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    v-model="loginForm.email"
+                />
             </label>
             <label for="password">
                 Password
                 {{ errors.password }}
-                <input type="password" name="password" id="password" v-model="loginForm.password"/>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    v-model="loginForm.password"
+                />
             </label>
             <label for="remember">
                 Remember me
-                <input type="checkbox" name="remember" id="remember" v-model="loginForm.remember"/>
+                <input
+                    type="checkbox"
+                    name="remember"
+                    id="remember"
+                    v-model="loginForm.remember"
+                />
             </label>
-            <input type="submit" value="Login"/>
+            <input type="submit" value="Login" />
         </form>
     </Layout>
-
 </template>
--- resources/js/pages/Auth/Register.vue
+++ resources/js/pages/Auth/Register.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import Layout from "@/layouts/Layout.vue";
-import {router, useForm, usePage} from "@inertiajs/vue3";
-import register from "@/routes/register";
-import {home} from "@/routes";
-import {computed} from "vue";
+import Layout from '@/layouts/Layout.vue';
+import { router, useForm, usePage } from '@inertiajs/vue3';
+import register from '@/routes/register';
+import { home } from '@/routes';
+import { computed } from 'vue';
 
 const loginForm = useForm({
-    name: "",
-    email: "",
-    password: "",
-    password_confirmation: "",
-})
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+});
 
 function submit() {
     loginForm.post(register.store().url, {
-        onSuccess: () => router.visit(home().url)
-    })
+        onSuccess: () => router.visit(home().url),
+    });
 }
 
 const page = usePage();
@@ -28,25 +28,44 @@ const errors = computed(() => page.props.errors ?? {});
             <label for="name">
                 Name
                 {{ errors.name }}
-                <input type="text" name="name" id="name" v-model="loginForm.name"/>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    v-model="loginForm.name"
+                />
             </label>
             <label for="email">
                 Email
                 {{ errors.email }}
-                <input type="email" name="email" id="email" v-model="loginForm.email"/>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    v-model="loginForm.email"
+                />
             </label>
             <label for="password">
                 Password
                 {{ errors.password }}
-                <input type="password" name="password" id="password" v-model="loginForm.password"/>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    v-model="loginForm.password"
+                />
             </label>
             <label for="password_confirmation">
                 Password
                 {{ errors.password_confirmation }}
-                <input type="password" name="password_confirmation" id="password_confirmation" v-model="loginForm.password_confirmation"/>
+                <input
+                    type="password"
+                    name="password_confirmation"
+                    id="password_confirmation"
+                    v-model="loginForm.password_confirmation"
+                />
             </label>
-            <input type="submit" value="Register"/>
+            <input type="submit" value="Register" />
         </form>
     </Layout>
-
 </template>
